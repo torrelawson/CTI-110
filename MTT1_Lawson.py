a858360212caef93d3146bf9dac50524eba72966
torrelawson <31484942+torrelawson@users.noreply.github.com>
2017-08-30 13:43:03 -0400

    Add files via upload

---
+++ MTT1_Lawson.py
@@ -0,0 +1,13 @@
+#CTI-110
+#M2TI -Sales Prediction
+#Lawson
+#30 August
+
+# Get the projected amount of total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the percentage
+profit = total_sales * 0.23
+
+# Display the profit
+print('The profit is $', format(profit, ',.2f'))
